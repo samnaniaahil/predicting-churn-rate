6762c96eaf901720c4b30d66c5e9d049a7185c76
samnaniaahil <37119335+samnaniaahil@users.noreply.github.com>
2019-12-07 11:37:37 -0500

    Update ann.py

--- ann.py
+++ ann.py
@@ -9,17 +9,21 @@ import pandas as pd
 
 
 # Importing the dataset
-dataset = pd.read_csv('file:///C:/Users/firoj/Desktop/DeepLearningAtoZ/P16-Artificial-Neural-Networks/Artificial_Neural_Networks/Churn_Modelling.csv')
+dataset = pd.read_csv('Churn_Modelling.csv')
+# Excluding first 3 independent variables from dataset (row#, customer ID and surname) as they don't affect the output of the ANN (dependent variale))
 X = dataset.iloc[:, 3: 13].values
 y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
-# Encoding the Independent Variable
+# Encoding independent variables (since they are strings)
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# Country 
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+# Gender
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+# Creates a dummy variable for index #1 (country)
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
@@ -28,7 +32,7 @@ X = X[:, 1:]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+# Feature Scaling - initially giving independent variables an equal weight factor in determining the dependent variable
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
@@ -45,29 +49,32 @@ from keras.layers import Dropout
 # Initializing the ANN
 classifier = Sequential()
 
-# Adding the input layer and the first hidden layer with Dropout
+# Adding the input layer and the first hidden layer (rectifier function) with Dropout
+# 11 nodes in input layer, 6 nodes in each hidden layer
 classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
 # Droput regularization for potential overfitting
 classifier.add(Dropout(0.1))
 
-# Adding second hidden layer
+# Adding second hidden layer (recitfier function) with Dropout
 classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
 # Droput regularization for potential overfitting
 classifier.add(Dropout(0.1))
 
-# Adding output layer
+# Adding output layer (sigmoid function)
+# 1 node in output layer
 classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
 
-# Compiling the ANN
+# Compiling the ANN with Gradient Descent
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy']) 
 
 # Fitting the ANN to the training set
 classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
 
-# Part 3 - Making the predictions and evaluating the model
+# Part 3 - Making predictions and evaluating the model
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+# Setting a thereshold for the prediction
 y_pred = (y_pred > 0.5)
 
 # Prediciting a new single observation
@@ -78,9 +85,21 @@ new_prediction = (new_prediction > 0.5)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
+# Trial Excercise 
+'''Geography: France
+Credit Score: 600
+Gender: Male
+Age: 40 years old
+Tenure: 3 years
+Balance: $60000
+Number of Products: 2
+Does this customer have a credit card ? Yes
+Is this customer an Active Member: Yes
+Estimated Salary: $50000'''
+
 # Part 4 - Evaluating, Improving and Tuning the ANN
 
-#Evaluating
+#Evaluating the ANN with K-fold Cross Validation
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from keras.models import Sequential
@@ -101,7 +120,6 @@ variance = accuracies.std()
 
 # Improving the ANN 
 # Droput regularization for potential overfitting (line 51 & 56)
-
 # Tuning the ANN with grid search
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV
